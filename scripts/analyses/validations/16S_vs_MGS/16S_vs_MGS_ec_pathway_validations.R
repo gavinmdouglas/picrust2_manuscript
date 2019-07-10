d75104fa6a90f341769b203122ebc47b3a730140
Gavin Douglas <gavinmdouglas@gmail.com>
2019-07-10 16:28:28 -0300

    Re-added PAPRICA EC numbers to output RDS

--- scripts/analyses/validations/16S_vs_MGS/16S_vs_MGS_ec_pathway_validations.R
+++ scripts/analyses/validations/16S_vs_MGS/16S_vs_MGS_ec_pathway_validations.R
@@ -75,6 +75,7 @@ hmp_ec_picrust2_nsti0.5_vs_mgs <- cor_all_cols(tab1 = hmp_ec_infiles$all_ecs_ove
 hmp_ec_picrust2_nsti0.25_vs_mgs <- cor_all_cols(tab1 = hmp_ec_infiles$all_ecs_overlap$picrust2_ec_nsti0.25, tab2 = hmp_ec_infiles$all_ecs_overlap$mgs_ec, cat_string="NSTI=0.25", metric="spearman")
 hmp_ec_picrust2_nsti0.1_vs_mgs <- cor_all_cols(tab1 = hmp_ec_infiles$all_ecs_overlap$picrust2_ec_nsti0.1, tab2 = hmp_ec_infiles$all_ecs_overlap$mgs_ec, cat_string="NSTI=0.1", metric="spearman")
 hmp_ec_picrust2_nsti0.05_vs_mgs <- cor_all_cols(tab1 = hmp_ec_infiles$all_ecs_overlap$picrust2_ec_nsti0.05, tab2 = hmp_ec_infiles$all_ecs_overlap$mgs_ec, cat_string="NSTI=0.05", metric="spearman")
+hmp_ec_paprica_vs_mgs <- cor_all_cols(tab1 = hmp_ec_infiles$all_ecs_overlap$paprica_ec, tab2 = hmp_ec_infiles$all_ecs_overlap$mgs_ec, cat_string="PAPRICA", metric="spearman")
 
 hmp_pathabun_mgs_null <- cor_all_cols(tab1 = hmp_pathabun_mgs_null_df, tab2 = hmp_pathway_infiles$all_pathabun$mgs_pathabun, cat_string="Null", metric="spearman")
 hmp_pathabun_picrust2_nsti2_gg_vs_mgs <- cor_all_cols(tab1 = hmp_pathway_infiles$all_pathabun$picrust2_pathabun_nsti2_gg, tab2 = hmp_pathway_infiles$all_pathabun$mgs_pathabun, cat_string="NSTI=2 (GG)", metric="spearman")
@@ -105,6 +106,7 @@ hmp_picrust2_ec_nsti0.5_metrics <- calc_accuracy_metrics(hmp_ec_infiles$all_ecs_
 hmp_picrust2_ec_nsti0.25_metrics <- calc_accuracy_metrics(hmp_ec_infiles$all_ecs_overlap$mgs_ec, hmp_ec_infiles$all_ecs_overlap$picrust2_ec_nsti0.25, category="NSTI=0.25")
 hmp_picrust2_ec_nsti0.1_metrics <- calc_accuracy_metrics(hmp_ec_infiles$all_ecs_overlap$mgs_ec, hmp_ec_infiles$all_ecs_overlap$picrust2_ec_nsti0.1, category="NSTI=0.1")
 hmp_picrust2_ec_nsti0.05_metrics <- calc_accuracy_metrics(hmp_ec_infiles$all_ecs_overlap$mgs_ec, hmp_ec_infiles$all_ecs_overlap$picrust2_ec_nsti0.05, category="NSTI=0.05")
+hmp_paprica_ec_metrics <- calc_accuracy_metrics(hmp_ec_infiles$all_ecs_overlap$mgs_ec, hmp_ec_infiles$all_ecs_overlap$paprica_ec, category="PAPRICA")
 hmp_null_ec_metrics <- calc_accuracy_metrics(hmp_ec_infiles$all_ecs_overlap$mgs_ec, hmp_ec_mgs_null_df_round, category="Null")
 
 hmp_picrust2_pathabun_nsti2_gg_metrics <- calc_accuracy_metrics(hmp_pathway_infiles$all_pathabun$mgs_pathabun, hmp_pathway_infiles$all_pathabun$picrust2_pathabun_nsti2_gg, category="NSTI=2 (GG)")
@@ -128,7 +130,7 @@ hmp_picrust2_pathcov_nsti0.05_metrics <- calc_accuracy_metrics(hmp_pathway_infil
 hmp_null_pathcov_metrics <- calc_accuracy_metrics(hmp_pathway_infiles$all_pathcov$mgs_pathcov, hmp_pathcov_mgs_null_df_round, category="Null")
 
 # Create lists of spearman correlation and accuracy metrics.
-hmp_ec_spearman_df <- rbind(hmp_ec_mgs_null, hmp_ec_picrust2_nsti2_gg_vs_mgs, hmp_ec_picrust2_nsti2_vs_mgs,
+hmp_ec_spearman_df <- rbind(hmp_ec_mgs_null, hmp_ec_paprica_vs_mgs, hmp_ec_picrust2_nsti2_gg_vs_mgs, hmp_ec_picrust2_nsti2_vs_mgs,
                             hmp_ec_picrust2_nsti1.5_vs_mgs, hmp_ec_picrust2_nsti1_vs_mgs, hmp_ec_picrust2_nsti0.5_vs_mgs,
                             hmp_ec_picrust2_nsti0.25_vs_mgs, hmp_ec_picrust2_nsti0.1_vs_mgs, hmp_ec_picrust2_nsti0.05_vs_mgs)
 
@@ -141,6 +143,7 @@ hmp_pathcov_spearman_df <- rbind(hmp_pathcov_mgs_null, hmp_pathcov_picrust2_nsti
                             hmp_pathcov_picrust2_nsti0.25_vs_mgs, hmp_pathcov_picrust2_nsti0.1_vs_mgs, hmp_pathcov_picrust2_nsti0.05_vs_mgs)
 
 hmp_ec_acc <- rbind(hmp_null_ec_metrics,
+                    hmp_paprica_ec_metrics,
                  hmp_picrust2_ec_nsti2_gg_metrics,
                  hmp_picrust2_ec_nsti2_metrics,
                  hmp_picrust2_ec_nsti1.5_metrics, 
@@ -182,6 +185,7 @@ mammal_ec_picrust2_nsti0.5_vs_mgs <- cor_all_cols(tab1 = mammal_ec_infiles$all_e
 mammal_ec_picrust2_nsti0.25_vs_mgs <- cor_all_cols(tab1 = mammal_ec_infiles$all_ecs_overlap$picrust2_ec_nsti0.25, tab2 = mammal_ec_infiles$all_ecs_overlap$mgs_ec, cat_string="NSTI=0.25", metric="spearman")
 mammal_ec_picrust2_nsti0.1_vs_mgs <- cor_all_cols(tab1 = mammal_ec_infiles$all_ecs_overlap$picrust2_ec_nsti0.1, tab2 = mammal_ec_infiles$all_ecs_overlap$mgs_ec, cat_string="NSTI=0.1", metric="spearman")
 mammal_ec_picrust2_nsti0.05_vs_mgs <- cor_all_cols(tab1 = mammal_ec_infiles$all_ecs_overlap$picrust2_ec_nsti0.05, tab2 = mammal_ec_infiles$all_ecs_overlap$mgs_ec, cat_string="NSTI=0.05", metric="spearman")
+mammal_ec_paprica_vs_mgs <- cor_all_cols(tab1 = mammal_ec_infiles$all_ecs_overlap$paprica_ec, tab2 = mammal_ec_infiles$all_ecs_overlap$mgs_ec, cat_string="PAPRICA", metric="spearman")
 
 mammal_pathabun_mgs_null <- cor_all_cols(tab1 = mammal_pathabun_mgs_null_df, tab2 = mammal_pathway_infiles$all_pathabun$mgs_pathabun, cat_string="Null", metric="spearman")
 mammal_pathabun_picrust2_nsti2_gg_vs_mgs <- cor_all_cols(tab1 = mammal_pathway_infiles$all_pathabun$picrust2_pathabun_nsti2_gg, tab2 = mammal_pathway_infiles$all_pathabun$mgs_pathabun, cat_string="NSTI=2 (GG)", metric="spearman")
@@ -212,6 +216,7 @@ mammal_picrust2_ec_nsti0.5_metrics <- calc_accuracy_metrics(mammal_ec_infiles$al
 mammal_picrust2_ec_nsti0.25_metrics <- calc_accuracy_metrics(mammal_ec_infiles$all_ecs_overlap$mgs_ec, mammal_ec_infiles$all_ecs_overlap$picrust2_ec_nsti0.25, category="NSTI=0.25")
 mammal_picrust2_ec_nsti0.1_metrics <- calc_accuracy_metrics(mammal_ec_infiles$all_ecs_overlap$mgs_ec, mammal_ec_infiles$all_ecs_overlap$picrust2_ec_nsti0.1, category="NSTI=0.1")
 mammal_picrust2_ec_nsti0.05_metrics <- calc_accuracy_metrics(mammal_ec_infiles$all_ecs_overlap$mgs_ec, mammal_ec_infiles$all_ecs_overlap$picrust2_ec_nsti0.05, category="NSTI=0.05")
+mammal_paprica_ec_metrics <- calc_accuracy_metrics(mammal_ec_infiles$all_ecs_overlap$mgs_ec, mammal_ec_infiles$all_ecs_overlap$paprica_ec, category="PAPRICA")
 mammal_null_ec_metrics <- calc_accuracy_metrics(mammal_ec_infiles$all_ecs_overlap$mgs_ec, mammal_ec_mgs_null_df_round, category="Null")
 
 mammal_picrust2_pathabun_nsti2_gg_metrics <- calc_accuracy_metrics(mammal_pathway_infiles$all_pathabun$mgs_pathabun, mammal_pathway_infiles$all_pathabun$picrust2_pathabun_nsti2_gg, category="NSTI=2 (GG)")
@@ -235,7 +240,7 @@ mammal_picrust2_pathcov_nsti0.05_metrics <- calc_accuracy_metrics(mammal_pathway
 mammal_null_pathcov_metrics <- calc_accuracy_metrics(mammal_pathway_infiles$all_pathcov$mgs_pathcov, mammal_pathcov_mgs_null_df_round, category="Null")
 
 # Create lists of spearman correlation and accuracy metrics.
-mammal_ec_spearman_df <- rbind(mammal_ec_mgs_null, mammal_ec_picrust2_nsti2_gg_vs_mgs, mammal_ec_picrust2_nsti2_vs_mgs,
+mammal_ec_spearman_df <- rbind(mammal_ec_mgs_null, mammal_ec_paprica_vs_mgs, mammal_ec_picrust2_nsti2_gg_vs_mgs, mammal_ec_picrust2_nsti2_vs_mgs,
                             mammal_ec_picrust2_nsti1.5_vs_mgs, mammal_ec_picrust2_nsti1_vs_mgs, mammal_ec_picrust2_nsti0.5_vs_mgs,
                             mammal_ec_picrust2_nsti0.25_vs_mgs, mammal_ec_picrust2_nsti0.1_vs_mgs, mammal_ec_picrust2_nsti0.05_vs_mgs)
 
@@ -248,6 +253,7 @@ mammal_pathcov_spearman_df <- rbind(mammal_pathcov_mgs_null, mammal_pathcov_picr
                                  mammal_pathcov_picrust2_nsti0.25_vs_mgs, mammal_pathcov_picrust2_nsti0.1_vs_mgs, mammal_pathcov_picrust2_nsti0.05_vs_mgs)
 
 mammal_ec_acc <- rbind(mammal_null_ec_metrics,
+                       mammal_paprica_ec_metrics,
                     mammal_picrust2_ec_nsti2_gg_metrics,
                     mammal_picrust2_ec_nsti2_metrics,
                     mammal_picrust2_ec_nsti1.5_metrics, 
@@ -286,6 +292,7 @@ ocean_ec_picrust2_nsti0.5_vs_mgs <- cor_all_cols(tab1 = ocean_ec_infiles$all_ecs
 ocean_ec_picrust2_nsti0.25_vs_mgs <- cor_all_cols(tab1 = ocean_ec_infiles$all_ecs_overlap$picrust2_ec_nsti0.25, tab2 = ocean_ec_infiles$all_ecs_overlap$mgs_ec, cat_string="NSTI=0.25", metric="spearman")
 ocean_ec_picrust2_nsti0.1_vs_mgs <- cor_all_cols(tab1 = ocean_ec_infiles$all_ecs_overlap$picrust2_ec_nsti0.1, tab2 = ocean_ec_infiles$all_ecs_overlap$mgs_ec, cat_string="NSTI=0.1", metric="spearman")
 ocean_ec_picrust2_nsti0.05_vs_mgs <- cor_all_cols(tab1 = ocean_ec_infiles$all_ecs_overlap$picrust2_ec_nsti0.05, tab2 = ocean_ec_infiles$all_ecs_overlap$mgs_ec, cat_string="NSTI=0.05", metric="spearman")
+ocean_ec_paprica_vs_mgs <- cor_all_cols(tab1 = ocean_ec_infiles$all_ecs_overlap$paprica_ec, tab2 = ocean_ec_infiles$all_ecs_overlap$mgs_ec, cat_string="PAPRICA", metric="spearman")
 
 ocean_pathabun_mgs_null <- cor_all_cols(tab1 = ocean_pathabun_mgs_null_df, tab2 = ocean_pathway_infiles$all_pathabun$mgs_pathabun, cat_string="Null", metric="spearman")
 ocean_pathabun_picrust2_nsti2_gg_vs_mgs <- cor_all_cols(tab1 = ocean_pathway_infiles$all_pathabun$picrust2_pathabun_nsti2_gg, tab2 = ocean_pathway_infiles$all_pathabun$mgs_pathabun, cat_string="NSTI=2 (GG)", metric="spearman")
@@ -316,6 +323,7 @@ ocean_picrust2_ec_nsti0.5_metrics <- calc_accuracy_metrics(ocean_ec_infiles$all_
 ocean_picrust2_ec_nsti0.25_metrics <- calc_accuracy_metrics(ocean_ec_infiles$all_ecs_overlap$mgs_ec, ocean_ec_infiles$all_ecs_overlap$picrust2_ec_nsti0.25, category="NSTI=0.25")
 ocean_picrust2_ec_nsti0.1_metrics <- calc_accuracy_metrics(ocean_ec_infiles$all_ecs_overlap$mgs_ec, ocean_ec_infiles$all_ecs_overlap$picrust2_ec_nsti0.1, category="NSTI=0.1")
 ocean_picrust2_ec_nsti0.05_metrics <- calc_accuracy_metrics(ocean_ec_infiles$all_ecs_overlap$mgs_ec, ocean_ec_infiles$all_ecs_overlap$picrust2_ec_nsti0.05, category="NSTI=0.05")
+ocean_paprica_ec_metrics <- calc_accuracy_metrics(ocean_ec_infiles$all_ecs_overlap$mgs_ec, ocean_ec_infiles$all_ecs_overlap$paprica_ec, category="PAPRICA")
 ocean_null_ec_metrics <- calc_accuracy_metrics(ocean_ec_infiles$all_ecs_overlap$mgs_ec, ocean_ec_mgs_null_df_round, category="Null")
 
 ocean_picrust2_pathabun_nsti2_gg_metrics <- calc_accuracy_metrics(ocean_pathway_infiles$all_pathabun$mgs_pathabun, ocean_pathway_infiles$all_pathabun$picrust2_pathabun_nsti2_gg, category="NSTI=2 (GG)")
@@ -339,7 +347,7 @@ ocean_picrust2_pathcov_nsti0.05_metrics <- calc_accuracy_metrics(ocean_pathway_i
 ocean_null_pathcov_metrics <- calc_accuracy_metrics(ocean_pathway_infiles$all_pathcov$mgs_pathcov, ocean_pathcov_mgs_null_df_round, category="Null")
 
 # Create lists of spearman correlation and accuracy metrics.
-ocean_ec_spearman_df <- rbind(ocean_ec_mgs_null, ocean_ec_picrust2_nsti2_gg_vs_mgs, ocean_ec_picrust2_nsti2_vs_mgs,
+ocean_ec_spearman_df <- rbind(ocean_ec_mgs_null, ocean_ec_paprica_vs_mgs, ocean_ec_picrust2_nsti2_gg_vs_mgs, ocean_ec_picrust2_nsti2_vs_mgs,
                             ocean_ec_picrust2_nsti1.5_vs_mgs, ocean_ec_picrust2_nsti1_vs_mgs, ocean_ec_picrust2_nsti0.5_vs_mgs,
                             ocean_ec_picrust2_nsti0.25_vs_mgs, ocean_ec_picrust2_nsti0.1_vs_mgs, ocean_ec_picrust2_nsti0.05_vs_mgs)
 
@@ -352,6 +360,7 @@ ocean_pathcov_spearman_df <- rbind(ocean_pathcov_mgs_null, ocean_pathcov_picrust
                                  ocean_pathcov_picrust2_nsti0.25_vs_mgs, ocean_pathcov_picrust2_nsti0.1_vs_mgs, ocean_pathcov_picrust2_nsti0.05_vs_mgs)
 
 ocean_ec_acc <- rbind(ocean_null_ec_metrics,
+                      ocean_paprica_ec_metrics,
                     ocean_picrust2_ec_nsti2_gg_metrics,
                     ocean_picrust2_ec_nsti2_metrics,
                     ocean_picrust2_ec_nsti1.5_metrics, 
@@ -391,6 +400,7 @@ blueberry_ec_picrust2_nsti0.5_vs_mgs <- cor_all_cols(tab1 = blueberry_ec_infiles
 blueberry_ec_picrust2_nsti0.25_vs_mgs <- cor_all_cols(tab1 = blueberry_ec_infiles$all_ecs_overlap$picrust2_ec_nsti0.25, tab2 = blueberry_ec_infiles$all_ecs_overlap$mgs_ec, cat_string="NSTI=0.25", metric="spearman")
 blueberry_ec_picrust2_nsti0.1_vs_mgs <- cor_all_cols(tab1 = blueberry_ec_infiles$all_ecs_overlap$picrust2_ec_nsti0.1, tab2 = blueberry_ec_infiles$all_ecs_overlap$mgs_ec, cat_string="NSTI=0.1", metric="spearman")
 blueberry_ec_picrust2_nsti0.05_vs_mgs <- cor_all_cols(tab1 = blueberry_ec_infiles$all_ecs_overlap$picrust2_ec_nsti0.05, tab2 = blueberry_ec_infiles$all_ecs_overlap$mgs_ec, cat_string="NSTI=0.05", metric="spearman")
+blueberry_ec_paprica_vs_mgs <- cor_all_cols(tab1 = blueberry_ec_infiles$all_ecs_overlap$paprica_ec, tab2 = blueberry_ec_infiles$all_ecs_overlap$mgs_ec, cat_string="PAPRICA", metric="spearman")
 
 blueberry_pathabun_mgs_null <- cor_all_cols(tab1 = blueberry_pathabun_mgs_null_df, tab2 = blueberry_pathway_infiles$all_pathabun$mgs_pathabun, cat_string="Null", metric="spearman")
 blueberry_pathabun_picrust2_nsti2_gg_vs_mgs <- cor_all_cols(tab1 = blueberry_pathway_infiles$all_pathabun$picrust2_pathabun_nsti2_gg, tab2 = blueberry_pathway_infiles$all_pathabun$mgs_pathabun, cat_string="NSTI=2 (GG)", metric="spearman")
@@ -421,6 +431,7 @@ blueberry_picrust2_ec_nsti0.5_metrics <- calc_accuracy_metrics(blueberry_ec_infi
 blueberry_picrust2_ec_nsti0.25_metrics <- calc_accuracy_metrics(blueberry_ec_infiles$all_ecs_overlap$mgs_ec, blueberry_ec_infiles$all_ecs_overlap$picrust2_ec_nsti0.25, category="NSTI=0.25")
 blueberry_picrust2_ec_nsti0.1_metrics <- calc_accuracy_metrics(blueberry_ec_infiles$all_ecs_overlap$mgs_ec, blueberry_ec_infiles$all_ecs_overlap$picrust2_ec_nsti0.1, category="NSTI=0.1")
 blueberry_picrust2_ec_nsti0.05_metrics <- calc_accuracy_metrics(blueberry_ec_infiles$all_ecs_overlap$mgs_ec, blueberry_ec_infiles$all_ecs_overlap$picrust2_ec_nsti0.05, category="NSTI=0.05")
+blueberry_paprica_ec_metrics <- calc_accuracy_metrics(blueberry_ec_infiles$all_ecs_overlap$mgs_ec, blueberry_ec_infiles$all_ecs_overlap$paprica_ec, category="PAPRICA")
 blueberry_null_ec_metrics <- calc_accuracy_metrics(blueberry_ec_infiles$all_ecs_overlap$mgs_ec, blueberry_ec_mgs_null_df_round, category="Null")
 
 blueberry_picrust2_pathabun_nsti2_gg_metrics <- calc_accuracy_metrics(blueberry_pathway_infiles$all_pathabun$mgs_pathabun, blueberry_pathway_infiles$all_pathabun$picrust2_pathabun_nsti2_gg, category="NSTI=2 (GG)")
@@ -444,7 +455,7 @@ blueberry_picrust2_pathcov_nsti0.05_metrics <- calc_accuracy_metrics(blueberry_p
 blueberry_null_pathcov_metrics <- calc_accuracy_metrics(blueberry_pathway_infiles$all_pathcov$mgs_pathcov, blueberry_pathcov_mgs_null_df_round, category="Null")
 
 # Create lists of spearman correlation and accuracy metrics.
-blueberry_ec_spearman_df <- rbind(blueberry_ec_mgs_null, blueberry_ec_picrust2_nsti2_gg_vs_mgs, blueberry_ec_picrust2_nsti2_vs_mgs,
+blueberry_ec_spearman_df <- rbind(blueberry_ec_mgs_null, blueberry_ec_paprica_vs_mgs, blueberry_ec_picrust2_nsti2_gg_vs_mgs, blueberry_ec_picrust2_nsti2_vs_mgs,
                             blueberry_ec_picrust2_nsti1.5_vs_mgs, blueberry_ec_picrust2_nsti1_vs_mgs, blueberry_ec_picrust2_nsti0.5_vs_mgs,
                             blueberry_ec_picrust2_nsti0.25_vs_mgs, blueberry_ec_picrust2_nsti0.1_vs_mgs, blueberry_ec_picrust2_nsti0.05_vs_mgs)
 
@@ -457,6 +468,7 @@ blueberry_pathcov_spearman_df <- rbind(blueberry_pathcov_mgs_null, blueberry_pat
                                  blueberry_pathcov_picrust2_nsti0.25_vs_mgs, blueberry_pathcov_picrust2_nsti0.1_vs_mgs, blueberry_pathcov_picrust2_nsti0.05_vs_mgs)
 
 blueberry_ec_acc <- rbind(blueberry_null_ec_metrics,
+                          blueberry_paprica_ec_metrics,
                     blueberry_picrust2_ec_nsti2_gg_metrics,
                     blueberry_picrust2_ec_nsti2_metrics,
                     blueberry_picrust2_ec_nsti1.5_metrics, 
