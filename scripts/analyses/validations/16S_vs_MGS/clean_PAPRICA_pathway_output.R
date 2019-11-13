86fb2558c3c9b6d058e2873c73b0d70d11330755
Gavin Douglas <gavinmdouglas@gmail.com>
2019-11-13 15:00:59 -0400

    updated paprica pathway cleaning script

--- scripts/analyses/validations/16S_vs_MGS/clean_PAPRICA_pathway_output.R
+++ scripts/analyses/validations/16S_vs_MGS/clean_PAPRICA_pathway_output.R
@@ -1,9 +1,12 @@
 ### Make PAPRICA pathway abundance output consistent with other tables.
 
-path_description <- read.table(gzfile("/home/gavin/github_repos/picrust_repos/picrust2/picrust2/default_files/description_mapfiles/metacyc_pathways_info_prokaryotes.txt.gz"),
-                           header=FALSE, sep="\t", stringsAsFactors = FALSE, quote="", comment.char = "")
+rm(list=ls(all=TRUE))
+
+path_description <- read.table(gzfile("/home/gavin/github_repos/picrust_repos/picrust2/picrust2/default_files/description_mapfiles/metacyc_pathways_info.txt.gz"),
+                               header=FALSE, sep="\t", stringsAsFactors = FALSE, quote="", comment.char = "")
 
 rownames(path_description) <- path_description$V2
+
 
 
 clean_write_paprica_pathabun <- function(infile, outfile, path_descrip) {
@@ -12,33 +15,34 @@ clean_write_paprica_pathabun <- function(infile, outfile, path_descrip) {
   colnames(paprica_pathabun) <- gsub("_rerep_seqs.", "", colnames(paprica_pathabun))
   
   paprica_pathabun <- paprica_pathabun[which(rownames(paprica_pathabun) %in% path_descrip$V2), ]
-
+  
   rownames(paprica_pathabun) <- path_descrip[rownames(paprica_pathabun), "V1"]
   
   write.table(x = paprica_pathabun, file = outfile, row.names=TRUE, col.names=NA, quote=FALSE, sep="\t")
 }
 
+datasets_to_paprica_path_out <- list()
 
-clean_write_paprica_pathabun(infile="/home/gavin/projects/picrust_pipeline/data/validation/hmp/16S/paprica_out/hmp_paprica_out.path_tally.csv",
-                             outfile="/home/gavin/projects/picrust_pipeline/data/validation/hmp/16S/paprica_out/hmp_paprica_out.path_tally_clean.csv",
-                             path_descrip=path_description)
-                             
+datasets_to_paprica_path_out[["cameroon"]] <- "/home/gavin/projects/picrust_pipeline/data/validation/cameroon/16S_workflow/paprica_working/combined.path_tally.csv"
+datasets_to_paprica_path_out[["hmp"]] <- "/home/gavin/projects/picrust_pipeline/data/validation/hmp/16S/paprica_working/combined.path_tally.csv"
+datasets_to_paprica_path_out[["indian"]] <- "/home/gavin/projects/picrust_pipeline/data/validation/indian/16S_workflow/paprica_working/combined.path_tally.csv"
+datasets_to_paprica_path_out[["primate"]] <- "/home/gavin/projects/picrust_pipeline/data/validation/primate/16S/paprica_working/combined.path_tally.csv"
+datasets_to_paprica_path_out[["mammal"]] <- "/home/gavin/projects/picrust_pipeline/data/validation/iGEM/16S/paprica_working/combined.path_tally.csv"
+datasets_to_paprica_path_out[["ocean"]] <- "/home/gavin/projects/picrust_pipeline/data/validation/ocean/16S/paprica_working/combined.path_tally.csv"
+datasets_to_paprica_path_out[["blueberry"]] <- "/home/gavin/projects/picrust_pipeline/data/validation/blueberry/16S/paprica_working/combined.path_tally.csv"
 
-clean_write_paprica_pathabun(infile="/home/gavin/projects/picrust_pipeline/data/validation/iGEM/16S/paprica_out/iGEM_paprica_out.path_tally.csv",
-                             outfile="/home/gavin/projects/picrust_pipeline/data/validation/iGEM/16S/paprica_out/iGEM_paprica_out.path_tally_clean.csv",
-                             path_descrip=path_description)
+datasets <- c("cameroon", "hmp", "indian", "primate", "mammal", "ocean", "blueberry")
 
-clean_write_paprica_pathabun(infile="/home/gavin/projects/picrust_pipeline/data/validation/ocean/16S/paprica_out/ocean_paprica_out.path_tally.csv",
-                             outfile="/home/gavin/projects/picrust_pipeline/data/validation/ocean/16S/paprica_out/ocean_paprica_out.path_tally_clean.csv",
-                             path_descrip=path_description)
-
-clean_write_paprica_pathabun(infile="/home/gavin/projects/picrust_pipeline/data/validation/blueberry/16S/paprica_out/blueberry_paprica_out.path_tally.csv",
-                             outfile="/home/gavin/projects/picrust_pipeline/data/validation/blueberry/16S/paprica_out/blueberry_paprica_out.path_tally_clean.csv",
-                             path_descrip=path_description)
+for(d in datasets) {
+  
+  clean_write_paprica_pathabun(infile=datasets_to_paprica_path_out[[d]],
+                               outfile=paste("/home/gavin/gavin_backup/projects/picrust2_manuscript/data/16S_validation/paprica_out/", d, "_paprica_path.tsv", sep=""),
+                               path_descrip=path_description)
+}
 
 
 # Also get all possible PAPRICA MetaCyc pathways predicted using similar approach:
-paprica_ref_pathabun <- read.table("/home/gavin/local/anaconda2_old_backup/envs/paprica/download/paprica/ref_genome_database/bacteria/terminal_paths.csv",
+paprica_ref_pathabun <- read.table(gzfile("/home/gavin/local/prg/paprica_v0.5.2/ref_genome_database/bacteria/terminal_paths.csv.gz"),
                                    header=TRUE, sep=",", row.names=1, check.names=FALSE, stringsAsFactors = FALSE, comment.char="", quote="\"")
 
 colnames(paprica_ref_pathabun)[which(! colnames(paprica_ref_pathabun) %in% rownames(path_description))]
@@ -46,7 +50,7 @@ colnames(paprica_ref_pathabun)[which(! colnames(paprica_ref_pathabun) %in% rowna
 paprica_ref_pathabun_subset <- paprica_ref_pathabun[, which(colnames(paprica_ref_pathabun) %in% rownames(path_description))]
 
 write.table(x = path_description[colnames(paprica_ref_pathabun_subset), "V1"],
-            file = "/home/gavin/gavin_backup/projects/picrust2_manuscript/data/16S_validation/possible_metacyc_pathways/paprica_bacteria_pathways.txt",
+            file = "/home/gavin/gavin_backup/projects/picrust2_manuscript/data/16S_validation/possible_path/paprica_path.txt",
             quote=FALSE, row.names=FALSE, col.names=FALSE)
 
 
