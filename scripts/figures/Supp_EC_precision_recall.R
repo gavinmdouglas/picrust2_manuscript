b0869c8e78eec9fdfb4f57de7081142500c43d9c
Gavin Douglas <gavinmdouglas@gmail.com>
2019-11-15 13:52:33 -0400

    re-made supp EC plots

--- scripts/figures/Supp_EC_precision_recall.R
+++ scripts/figures/Supp_EC_precision_recall.R
@@ -1,9 +1,7 @@
-### Code to make figure plotting accuracy metrics across HSP tools on each 16S validation dataset.
-### These metrics were calculated by assuming the metagenomics sequencing was the "gold standard".
-### Also tests for statistical significance between these categories (and save wilcoxon output to RDS).
 
 rm(list=ls(all=TRUE))
 
+library(cowplot)
 library(ggplot2)
 library(reshape2)
 library(ggpubr)
@@ -15,106 +13,58 @@ source("/home/gavin/gavin_backup/projects/picrust2_manuscript/scripts/picrust2_m
 
 extra_nsti_categories <- c("NSTI=1.5", "NSTI=1", "NSTI=0.5", "NSTI=0.25", "NSTI=0.1", "NSTI=0.05")
 
-# Read in metrics and prep per dataset.
-# HMP:
-hmp_ec_precision_outlist <- parse_acc_metrics_rds_and_calc_wilcoxon(acc_rds = "hmp_ec_acc_metrics.rds",
-                                                                    dataset_name = "HMP",
-                                                                    metric_col="precision",
-                                                                    wilcox_cat2ignore = extra_nsti_categories,
-                                                                    y_pos_start = 1.03)
+ec_precision_raw <- list()
+ec_precision <- list()
+ec_precision_wilcoxon <- list()
 
-hmp_ec_recall_outlist <- parse_acc_metrics_rds_and_calc_wilcoxon(acc_rds = "hmp_ec_acc_metrics.rds",
-                                                                 dataset_name = "HMP",
-                                                                 metric_col="recall",
-                                                                 wilcox_cat2ignore = extra_nsti_categories,
-                                                                 y_pos_start = 1.03)
+ec_recall_raw <- list()
+ec_recall <- list()
+ec_recall_wilcoxon <- list()
 
+datasets <- c("cameroon", "primate", "hmp", "mammal", "ocean", "blueberry", "indian")
 
-hmp_ec_precision <- hmp_ec_precision_outlist[[1]]
-hmp_ec_precision_wilcoxon <- hmp_ec_precision_outlist[[2]]
+dataset2name <- list("cameroon"="Cameroon", "indian"="Indian", "hmp"="HMP", "mammal"="Mammal",
+                     "ocean"="Ocean", "blueberry"="Soil (Blueberry)", "primate"="Primate")
 
-hmp_ec_recall <- hmp_ec_recall_outlist[[1]]
-hmp_ec_recall_wilcoxon <- hmp_ec_recall_outlist[[2]]
-
-
-# Mammal:
-
-mammal_ec_precision_outlist <- parse_acc_metrics_rds_and_calc_wilcoxon(acc_rds = "mammal_ec_acc_metrics.rds",
-                                                                    dataset_name = "Mammal",
-                                                                    metric_col="precision",
-                                                                    wilcox_cat2ignore = extra_nsti_categories,
-                                                                    y_pos_start = 1.03)
-
-mammal_ec_recall_outlist <- parse_acc_metrics_rds_and_calc_wilcoxon(acc_rds = "mammal_ec_acc_metrics.rds",
-                                                                 dataset_name = "Mammal",
-                                                                 metric_col="recall",
-                                                                 wilcox_cat2ignore = extra_nsti_categories,
-                                                                 y_pos_start = 1.03)
-
-
-
-mammal_ec_precision <- mammal_ec_precision_outlist[[1]]
-mammal_ec_precision_wilcoxon <- mammal_ec_precision_outlist[[2]]
-
-mammal_ec_recall <- mammal_ec_recall_outlist[[1]]
-mammal_ec_recall_wilcoxon <- mammal_ec_recall_outlist[[2]]
-
-
-# Ocean:
-
-ocean_ec_precision_outlist <- parse_acc_metrics_rds_and_calc_wilcoxon(acc_rds = "ocean_ec_acc_metrics.rds",
-                                                                    dataset_name = "Ocean",
-                                                                    metric_col="precision",
-                                                                    wilcox_cat2ignore = extra_nsti_categories,
-                                                                    y_pos_start = 1.03)
-
-ocean_ec_recall_outlist <- parse_acc_metrics_rds_and_calc_wilcoxon(acc_rds = "ocean_ec_acc_metrics.rds",
-                                                                 dataset_name = "Ocean",
-                                                                 metric_col="recall",
-                                                                 wilcox_cat2ignore = extra_nsti_categories,
-                                                                 y_pos_start = 1.03)
-
-ocean_ec_precision <- ocean_ec_precision_outlist[[1]]
-ocean_ec_precision_wilcoxon <- ocean_ec_precision_outlist[[2]]
-
-ocean_ec_recall <- ocean_ec_recall_outlist[[1]]
-ocean_ec_recall_wilcoxon <- ocean_ec_recall_outlist[[2]]
-
-
-
-# Soil (Blueberry):
-
-blueberry_ec_precision_outlist <- parse_acc_metrics_rds_and_calc_wilcoxon(acc_rds = "blueberry_ec_acc_metrics.rds",
-                                                                    dataset_name = "Soil (Blueberry)",
-                                                                    metric_col="precision",
-                                                                    wilcox_cat2ignore = extra_nsti_categories,
-                                                                    y_pos_start = 1.03)
-
-blueberry_ec_recall_outlist <- parse_acc_metrics_rds_and_calc_wilcoxon(acc_rds = "blueberry_ec_acc_metrics.rds",
-                                                                 dataset_name = "Soil (Blueberry)",
-                                                                 metric_col="recall",
-                                                                 wilcox_cat2ignore = extra_nsti_categories,
-                                                                 y_pos_start = 1.03)
-
-blueberry_ec_precision <- blueberry_ec_precision_outlist[[1]]
-blueberry_ec_precision_wilcoxon <- blueberry_ec_precision_outlist[[2]]
-
-blueberry_ec_recall <- blueberry_ec_recall_outlist[[1]]
-blueberry_ec_recall_wilcoxon <- blueberry_ec_recall_outlist[[2]]
-
+for(d in datasets) {
+  
+  ec_precision_raw[[d]] <- parse_acc_metrics_rds_and_calc_wilcoxon(acc_rds = paste(d, "_ec_acc_df.rds", sep=""),
+                                                                   dataset_name = dataset2name[[d]],
+                                                                   metric_col="precision",
+                                                                   wilcox_cat2ignore = extra_nsti_categories,
+                                                                   y_pos_start = 1.03)
+  
+  ec_precision[[d]] <- ec_precision_raw[[d]][[1]]
+  ec_precision_wilcoxon[[d]] <- ec_precision_raw[[d]][[2]]
+  
+  
+  ec_recall_raw[[d]] <- parse_acc_metrics_rds_and_calc_wilcoxon(acc_rds = paste(d, "_ec_acc_df.rds", sep=""),
+                                                                dataset_name = dataset2name[[d]],
+                                                                metric_col="recall",
+                                                                wilcox_cat2ignore = extra_nsti_categories,
+                                                                y_pos_start = 1.03)
+  
+  ec_recall[[d]] <- ec_recall_raw[[d]][[1]]
+  ec_recall_wilcoxon[[d]] <- ec_recall_raw[[d]][[2]]
+  
+}
 
 # Precision
-combined_ec_precision <- rbind(hmp_ec_precision, mammal_ec_precision,
-                         ocean_ec_precision, blueberry_ec_precision)
+combined_ec_precision <- do.call("rbind", ec_precision)
 
-combined_ec_precision_wilcoxon <- rbind(hmp_ec_precision_wilcoxon, mammal_ec_precision_wilcoxon,
-                                  ocean_ec_precision_wilcoxon, blueberry_ec_precision_wilcoxon)
+combined_ec_precision_wilcoxon <- do.call("rbind", ec_precision_wilcoxon)
 
 combined_ec_precision$cat <- as.character(combined_ec_precision$cat)
+
+# Remove some NSTI categories so it's easier to plot.
+combined_ec_precision <- combined_ec_precision[-which(combined_ec_precision$cat %in% c("NSTI=1.5", "NSTI=0.5", "NSTI=0.25", "NSTI=0.1")), ]
+
 combined_ec_precision$cat <- factor(combined_ec_precision$cat,
-                                    levels=c("Null", "PAPRICA", "NSTI=2 (GG)", "NSTI=2", "NSTI=1.5",
-                                             "NSTI=1", "NSTI=0.5", "NSTI=0.25", "NSTI=0.1", "NSTI=0.05"))
+                                    levels=c("Null", "PAPRICA", "NSTI=2","NSTI=1","NSTI=0.05"))
 combined_ec_precision_melt <- melt(combined_ec_precision)
+
+combined_ec_precision_melt$dataset <- factor(combined_ec_precision_melt$dataset, levels=c("Cameroon", "Indian", "HMP", "Primate", "Mammal", "Ocean", "Soil (Blueberry)"))
+
 
 ec_precision_boxplots <- ggplot(combined_ec_precision_melt, aes(x=cat, y=value, fill=Database)) +
   geom_boxplot(outlier.shape = NA) +
@@ -124,24 +74,29 @@ ec_precision_boxplots <- ggplot(combined_ec_precision_melt, aes(x=cat, y=value, 
   xlab("") +
   guides(fill=FALSE) +
   facet_grid(. ~ dataset, scales = "free", space = "free", switch="x") +
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-        panel.background = element_blank(), axis.line = element_line(colour = "black"),
+  theme(panel.background = element_rect(fill = "gray90"),
+        axis.line = element_line(colour = "black"),
         axis.text.x=element_text(angle=45, hjust=1)) +
   scale_fill_manual(values=c("light grey", "#F8766D", "#00BFC4"))
 
 
 # Recall
-combined_ec_recall <- rbind(hmp_ec_recall, mammal_ec_recall,
-                               ocean_ec_recall, blueberry_ec_recall)
+combined_ec_recall <- do.call("rbind", ec_recall)
 
-combined_ec_recall_wilcoxon <- rbind(hmp_ec_recall_wilcoxon, mammal_ec_recall_wilcoxon,
-                                        ocean_ec_recall_wilcoxon, blueberry_ec_recall_wilcoxon)
+combined_ec_recall_wilcoxon <- do.call("rbind", ec_recall_wilcoxon)
 
 combined_ec_recall$cat <- as.character(combined_ec_recall$cat)
+combined_ec_recall <- combined_ec_recall[-which(combined_ec_recall$cat %in% c("NSTI=1.5", "NSTI=0.5", "NSTI=0.25", "NSTI=0.1")), ]
+
+
+
 combined_ec_recall$cat <- factor(combined_ec_recall$cat,
-                                 levels=c("Null", "PAPRICA", "NSTI=2 (GG)", "NSTI=2", "NSTI=1.5",
-                                          "NSTI=1", "NSTI=0.5", "NSTI=0.25", "NSTI=0.1", "NSTI=0.05"))
+                                 levels=c("Null", "PAPRICA", "NSTI=2",
+                                          "NSTI=1", "NSTI=0.05"))
 combined_ec_recall_melt <- melt(combined_ec_recall)
+
+combined_ec_recall_melt$dataset <- factor(combined_ec_recall_melt$dataset, levels=c("Cameroon", "Indian", "HMP", "Primate", "Mammal", "Ocean", "Soil (Blueberry)"))
+
 
 ec_recall_boxplots <- ggplot(combined_ec_recall_melt, aes(x=cat, y=value, fill=Database)) +
   geom_boxplot(outlier.shape = NA) +
@@ -151,33 +106,15 @@ ec_recall_boxplots <- ggplot(combined_ec_recall_melt, aes(x=cat, y=value, fill=D
   xlab("") +
   guides(fill=FALSE) +
   facet_grid(. ~ dataset, scales = "free", space = "free", switch="x") +
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-        panel.background = element_blank(), axis.line = element_line(colour = "black"),
+  theme(panel.background = element_rect(fill = "gray90"), axis.line = element_line(colour = "black"),
         axis.text.x=element_text(angle=45, hjust=1)) +
   scale_fill_manual(values=c("light grey", "#F8766D", "#00BFC4"))
 
+pdf(file = "../../../figures/Supp_EC_precision_recall.pdf", width=14, height=8)
 
-
-# 11 x 7
 plot_grid(ec_precision_boxplots,
           ec_recall_boxplots,
           nrow=2,
           ncol=1,
-          labels=c("A", "B"))
-
-
-# Report mean and sd per dataset.
-
-# For precision:
-mean(hmp_ec_precision[which(hmp_ec_precision$cat == "NSTI=2"), "precision"])
-sd(hmp_ec_precision[which(hmp_ec_precision$cat == "NSTI=2"), "precision"])
-
-mean(mammal_ec_precision[which(mammal_ec_precision$cat == "NSTI=2"), "precision"])
-sd(mammal_ec_precision[which(mammal_ec_precision$cat == "NSTI=2"), "precision"])
-
-mean(ocean_ec_precision[which(ocean_ec_precision$cat == "NSTI=2"), "precision"])
-sd(ocean_ec_precision[which(ocean_ec_precision$cat == "NSTI=2"), "precision"])
-
-mean(blueberry_ec_precision[which(blueberry_ec_precision$cat == "NSTI=2"), "precision"])
-sd(blueberry_ec_precision[which(blueberry_ec_precision$cat == "NSTI=2"), "precision"])
-
+          labels=c("a", "b"))
+dev.off()
--- scripts/figures/Supp_EC_spearman_w_NSTI.R
+++ scripts/figures/Supp_EC_spearman_w_NSTI.R
@@ -15,163 +15,72 @@ source("/home/gavin/gavin_backup/projects/picrust2_manuscript/scripts/picrust2_m
 
 extra_nsti_categories <- c("NSTI=1.5", "NSTI=1", "NSTI=0.5", "NSTI=0.25", "NSTI=0.1", "NSTI=0.05")
 
-# Read in metrics and prep per dataset.
-# HMP:
-hmp_ec_rho_outlist <- parse_rho_rds_and_calc_wilcoxon(rho_rds = "hmp_ec_spearman_df.rds",
-                                                      dataset_name = "HMP",
-                                                      wilcox_cat2ignore = extra_nsti_categories,
-                                                      y_pos_start = 0.97)
+datasets <- c("hmp", "mammal", "ocean", "blueberry", "indian", "cameroon", "primate")
+dataset_names <- c("HMP", "Mammal", "Ocean", "Soil (Blueberry)", "Indian", "Cameroon", "Primate")
 
+ec_rho_outlist <- list()
+ec_rho <- list()
+ec_rho_wilcoxon <- list()
 
-hmp_ec_rho <- hmp_ec_rho_outlist[[1]]
-hmp_ec_rho_wilcoxon <- hmp_ec_rho_outlist[[2]]
+for(i in 1:length(datasets)) {
+  
+  ec_rho_outlist[[datasets[i]]] <- parse_rho_rds_and_calc_wilcoxon(rho_rds = paste(datasets[i], "_ec_spearman_df.rds", sep=""),
+                                                                  dataset_name = dataset_names[i],
+                                                                  wilcox_cat2ignore = extra_nsti_categories,
+                                                                  y_pos_start = 0.9)
+  
+   ec_rho[[datasets[i]]] <- ec_rho_outlist[[datasets[i]]][[1]]
+   ec_rho_wilcoxon[[datasets[i]]] <- ec_rho_outlist[[datasets[i]]][[2]]
 
-
-# Mammal:
-mammal_ec_rho_outlist <- parse_rho_rds_and_calc_wilcoxon(rho_rds = "mammal_ec_spearman_df.rds",
-                                                      dataset_name = "Mammal",
-                                                      wilcox_cat2ignore = extra_nsti_categories,
-                                                      y_pos_start = 0.97)
-
-mammal_ec_rho <- mammal_ec_rho_outlist[[1]]
-mammal_ec_rho_wilcoxon <- mammal_ec_rho_outlist[[2]]
-
-
-# Ocean:
-ocean_ec_rho_outlist <- parse_rho_rds_and_calc_wilcoxon(rho_rds = "ocean_ec_spearman_df.rds",
-                                                      dataset_name = "Ocean",
-                                                      wilcox_cat2ignore = extra_nsti_categories,
-                                                      y_pos_start = 0.97)
-
-ocean_ec_rho <- ocean_ec_rho_outlist[[1]]
-ocean_ec_rho_wilcoxon <- ocean_ec_rho_outlist[[2]]
-
-
-# Soil (Blueberry):
-blueberry_ec_rho_outlist <- parse_rho_rds_and_calc_wilcoxon(rho_rds = "blueberry_ec_spearman_df.rds",
-                                                      dataset_name = "Soil (Blueberry)",
-                                                      wilcox_cat2ignore = extra_nsti_categories,
-                                                      y_pos_start = 0.97)
-
-
-blueberry_ec_rho <- blueberry_ec_rho_outlist[[1]]
-blueberry_ec_rho_wilcoxon <- blueberry_ec_rho_outlist[[2]]
-
-
+}
 
 # Make plot for each dataset.
+EC_spearman_boxplots <- list()
 
-hmp_ec_rho$cat <- as.character(hmp_ec_rho$cat)
-hmp_ec_rho$cat <- factor(hmp_ec_rho$cat,
-                         levels=c("Null", "PAPRICA", "NSTI=2 (GG)", "NSTI=2", "NSTI=1.5",
-                                  "NSTI=1", "NSTI=0.5", "NSTI=0.25", "NSTI=0.1", "NSTI=0.05"))
+for(j in 1:length(datasets)) {
+  
+  dataset_ec_rho <- ec_rho[[datasets[j]]]
+  
+  dataset_ec_rho$cat <- as.character(dataset_ec_rho$cat)
+  dataset_ec_rho <- dataset_ec_rho[-which(dataset_ec_rho$cat %in% c("NSTI=1.5",  "NSTI=0.5", "NSTI=0.25", "NSTI=0.1")), ]
+  
+  dataset_ec_rho$cat <- factor(dataset_ec_rho$cat,
+                         levels=c("Null", "PAPRICA", "NSTI=2", "NSTI=1", "NSTI=0.05"))
 
-hmp_ec_rho_melt <- melt(hmp_ec_rho)
+  dataset_ec_rho_melt <- melt(dataset_ec_rho)
 
-hmp_ec_rho_melt[which(hmp_ec_rho_melt$Database == "Other"), "Database"] <- "PAPRICA"
+  dataset_ec_rho_melt[which(dataset_ec_rho_melt$Database == "Other"), "Database"] <- "PAPRICA"
 
-hmp_ec_spearman_boxplots <- ggplot(hmp_ec_rho_melt, aes(x=cat, y=value, fill=Database)) +
-  geom_boxplot(outlier.shape = NA) +
-  geom_quasirandom(size=0.1) +
-  ylim(c(0.4, 1.31)) +
-  ylab(c("Spearman Correlation Coefficient")) +
-  xlab("") +
-  facet_grid(. ~ dataset, scales = "free", space = "free", switch="x") +
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-        panel.background = element_blank(), axis.line = element_line(colour = "black"),
-        axis.text.x=element_text(angle=45, hjust=1),
-        legend.position = c(0.6, 0.85), legend.background = element_rect(color = "black", 
-                                                                          fill = "white", size = 0.3, linetype = "solid"),
-        legend.title = element_text(colour="black", size=8, face="bold"),
-        legend.text = element_text(colour="black", size=8)) +
-  scale_fill_manual(values=c("light grey", "#F8766D", "#00BFC4")) +
-  stat_pvalue_manual(hmp_ec_rho_wilcoxon, label = "p_symbol")
+  EC_spearman_boxplots[[datasets[j]]] <- ggplot(dataset_ec_rho_melt, aes(x=cat, y=value, fill=Database)) +
+                                        geom_boxplot(outlier.shape = NA) +
+                                        geom_quasirandom(size=0.1) +
+                                        scale_y_continuous(breaks=c(0.4, 0.6, 0.8, 1.0), limits=c(0.4, 1)) +
+                                        ylab(c("Spearman Correlation Coefficient")) +
+                                        xlab("") +
+                                        facet_grid(. ~ dataset, scales = "free", space = "free", switch="x") +
+                                        theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+                                              panel.background = element_blank(), axis.line = element_line(colour = "black"),
+                                              axis.text.x=element_text(angle=45, hjust=1),
+                                              legend.position = c(0.6, 0.2), legend.background = element_rect(color = "black", 
+                                                                                                                fill = "white", size = 0.3, linetype = "solid"),
+                                              legend.title = element_text(colour="black", size=8, face="bold"),
+                                              legend.text = element_text(colour="black", size=8)) +
+                                        scale_fill_manual(values=c("light grey", "#F8766D", "#00BFC4")) +
+                                        stat_pvalue_manual(ec_rho_wilcoxon[[datasets[j]]], label = "p_symbol")
 
+}
 
-mammal_ec_rho$cat <- as.character(mammal_ec_rho$cat)
-mammal_ec_rho$cat <- factor(mammal_ec_rho$cat,
-                            levels=c("Null", "PAPRICA", "NSTI=2 (GG)", "NSTI=2", "NSTI=1.5",
-                                     "NSTI=1", "NSTI=0.5", "NSTI=0.25", "NSTI=0.1", "NSTI=0.05"))
+pdf(file = "../../../figures/Supp_EC_spearman.pdf", width=15, height=9)
 
-mammal_ec_rho_melt <- melt(mammal_ec_rho)
-
-mammal_ec_rho_melt[which(mammal_ec_rho_melt$Database == "Other"), "Database"] <- "PAPRICA"
-
-mammal_ec_spearman_boxplots <- ggplot(mammal_ec_rho_melt, aes(x=cat, y=value, fill=Database)) +
-  geom_boxplot(outlier.shape = NA) +
-  geom_quasirandom(size=0.1) +
-  ylim(c(0.4, 1.31)) +
-  ylab(c("Spearman Correlation Coefficient")) +
-  xlab("") +
-  facet_grid(. ~ dataset, scales = "free", space = "free", switch="x") +
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-        panel.background = element_blank(), axis.line = element_line(colour = "black"),
-        axis.text.x=element_text(angle=45, hjust=1),
-        legend.position = c(0.6, 0.85), legend.background = element_rect(color = "black", 
-                                                                         fill = "white", size = 0.3, linetype = "solid"),
-        legend.title = element_text(colour="black", size=8, face="bold"),
-        legend.text = element_text(colour="black", size=8)) +
-  scale_fill_manual(values=c("light grey", "#F8766D", "#00BFC4")) +
-  stat_pvalue_manual(mammal_ec_rho_wilcoxon, label = "p_symbol")
-
-
-ocean_ec_rho$cat <- as.character(ocean_ec_rho$cat)
-ocean_ec_rho$cat <- factor(ocean_ec_rho$cat,
-                           levels=c("Null", "PAPRICA", "NSTI=2 (GG)", "NSTI=2", "NSTI=1.5",
-                                    "NSTI=1", "NSTI=0.5", "NSTI=0.25", "NSTI=0.1", "NSTI=0.05"))
-
-ocean_ec_rho_melt <- melt(ocean_ec_rho)
-
-ocean_ec_rho_melt[which(ocean_ec_rho_melt$Database == "Other"), "Database"] <- "PAPRICA"
-
-ocean_ec_spearman_boxplots <- ggplot(ocean_ec_rho_melt, aes(x=cat, y=value, fill=Database)) +
-  geom_boxplot(outlier.shape = NA) +
-  geom_quasirandom(size=0.1) +
-  ylim(c(0.4, 1.31)) +
-  ylab(c("Spearman Correlation Coefficient")) +
-  xlab("") +
-  facet_grid(. ~ dataset, scales = "free", space = "free", switch="x") +
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-        panel.background = element_blank(), axis.line = element_line(colour = "black"),
-        axis.text.x=element_text(angle=45, hjust=1),
-        legend.position = c(0.6, 0.85), legend.background = element_rect(color = "black", 
-                                                                         fill = "white", size = 0.3, linetype = "solid"),
-        legend.title = element_text(colour="black", size=8, face="bold"),
-        legend.text = element_text(colour="black", size=8)) +
-  scale_fill_manual(values=c("light grey", "#F8766D", "#00BFC4")) +
-  stat_pvalue_manual(ocean_ec_rho_wilcoxon, label = "p_symbol")
-
-blueberry_ec_rho$cat <- as.character(blueberry_ec_rho$cat)
-blueberry_ec_rho$cat <- factor(blueberry_ec_rho$cat,
-                               levels=c("Null", "PAPRICA", "NSTI=2 (GG)", "NSTI=2", "NSTI=1.5",
-                                        "NSTI=1", "NSTI=0.5", "NSTI=0.25", "NSTI=0.1", "NSTI=0.05"))
-
-blueberry_ec_rho_melt <- melt(blueberry_ec_rho)
-
-blueberry_ec_rho_melt[which(blueberry_ec_rho_melt$Database == "Other"), "Database"] <- "PAPRICA"
-
-blueberry_ec_spearman_boxplots <- ggplot(blueberry_ec_rho_melt, aes(x=cat, y=value, fill=Database)) +
-  geom_boxplot(outlier.shape = NA) +
-  geom_quasirandom(size=0.1) +
-  ylim(c(0.4, 1.31)) +
-  ylab(c("Spearman Correlation Coefficient")) +
-  xlab("") +
-  facet_grid(. ~ dataset, scales = "free", space = "free", switch="x") +
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-        panel.background = element_blank(), axis.line = element_line(colour = "black"),
-        axis.text.x=element_text(angle=45, hjust=1),
-        legend.position = c(0.6, 0.85), legend.background = element_rect(color = "black", 
-                                                                         fill = "white", size = 0.3, linetype = "solid"),
-        legend.title = element_text(colour="black", size=8, face="bold"),
-        legend.text = element_text(colour="black", size=8)) +
-  scale_fill_manual(values=c("light grey", "#F8766D", "#00BFC4")) +
-  stat_pvalue_manual(blueberry_ec_rho_wilcoxon, label = "p_symbol")
-
-# 9 x 10
-plot_grid(hmp_ec_spearman_boxplots,
-          mammal_ec_spearman_boxplots,
-          ocean_ec_spearman_boxplots,
-          blueberry_ec_spearman_boxplots,
-          labels=c("A", "B", "C", "D"),
+plot_grid(EC_spearman_boxplots[["cameroon"]],
+          EC_spearman_boxplots[["hmp"]],
+          EC_spearman_boxplots[["indian"]],
+          EC_spearman_boxplots[["mammal"]],
+          EC_spearman_boxplots[["ocean"]],
+          EC_spearman_boxplots[["primate"]],
+          EC_spearman_boxplots[["blueberry"]],
+          labels=c("a", "b", "c", "d", "e", "f", "g"),
           nrow=2,
-          ncol=2)
+          ncol=4)
+
+dev.off()
