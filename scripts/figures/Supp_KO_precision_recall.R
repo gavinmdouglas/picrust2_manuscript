c3e3dada27f0e57d70cd1e5a571ffc2214b1cd99
Gavin Douglas <gavinmdouglas@gmail.com>
2019-11-18 14:52:55 -0400

    cleaned up KO prec and recall boxplots

--- scripts/figures/Supp_KO_precision_recall.R
+++ scripts/figures/Supp_KO_precision_recall.R
@@ -26,7 +26,7 @@ ko_recall_wilcoxon <- list()
 
 datasets <- c("cameroon", "primate", "hmp", "mammal", "ocean", "blueberry", "indian")
 
-dataset2name <- list("cameroon"="Cameroon", "indian"="India", "hmp"="HMP", "mammal"="Mammal",
+dataset2name <- list("cameroon"="Cameroonian", "indian"="Indian", "hmp"="HMP", "mammal"="Mammal",
                      "ocean"="Ocean", "blueberry"="Soil (Blueberry)", "primate"="Primate")
 
 for(d in datasets) {
@@ -67,13 +67,13 @@ combined_ko_precision$cat <- factor(combined_ko_precision$cat,
                                                       "NSTI=1.5", "NSTI=1", "NSTI=0.5", "NSTI=0.25", "NSTI=0.1", "NSTI=0.05"))
 combined_ko_precision_melt <- melt(combined_ko_precision)
 
-combined_ko_precision_melt$dataset <- factor(combined_ko_precision_melt$dataset, levels=c("Cameroon", "Indian", "HMP", "Primate", "Mammal", "Ocean", "Soil (Blueberry)"))
+combined_ko_precision_melt$dataset <- factor(combined_ko_precision_melt$dataset, levels=c("Cameroonian", "HMP", "Indian", "Mammal", "Ocean", "Primate", "Soil (Blueberry)"))
 
 
 ko_precision_boxplots <- ggplot(combined_ko_precision_melt, aes(x=cat, y=value, fill=Database)) +
   geom_boxplot(outlier.shape = NA) +
   geom_quasirandom(size=0.1) +
-  ylim(c(0.25, 1)) +
+  scale_y_continuous(breaks=c(0.4, 0.6, 0.8, 1.0), limits=c(0.25, 1.30)) +
   ylab(c("Precision")) +
   xlab("") +
   guides(fill=FALSE) +
@@ -81,7 +81,8 @@ ko_precision_boxplots <- ggplot(combined_ko_precision_melt, aes(x=cat, y=value, 
   theme(panel.background = element_rect(fill = "gray90"),
         axis.line = element_line(colour = "black"),
         axis.text.x=element_text(angle=45, hjust=1)) +
-  scale_fill_manual(values=c("light grey", "#F8766D", "#00BFC4"))
+  scale_fill_manual(values=c("light grey", "#F8766D", "#00BFC4")) +
+  stat_pvalue_manual(combined_ko_precision_wilcoxon, label = "p_symbol")
 
 
 # Recall
@@ -99,22 +100,23 @@ combined_ko_recall$cat <- factor(combined_ko_recall$cat,
                                              "NSTI=1.5", "NSTI=1", "NSTI=0.5", "NSTI=0.25", "NSTI=0.1", "NSTI=0.05"))
 combined_ko_recall_melt <- melt(combined_ko_recall)
 
-combined_ko_recall_melt$dataset <- factor(combined_ko_recall_melt$dataset, levels=c("Cameroon", "Indian", "HMP", "Primate", "Mammal", "Ocean", "Soil (Blueberry)"))
+combined_ko_recall_melt$dataset <- factor(combined_ko_recall_melt$dataset, levels=c("Cameroonian", "HMP", "Indian", "Mammal", "Ocean", "Primate", "Soil (Blueberry)"))
 
 
 ko_recall_boxplots <- ggplot(combined_ko_recall_melt, aes(x=cat, y=value, fill=Database)) +
   geom_boxplot(outlier.shape = NA) +
   geom_quasirandom(size=0.1) +
-  ylim(c(0.25, 1)) +
+  scale_y_continuous(breaks=c(0.4, 0.6, 0.8, 1.0), limits=c(0.25, 1.30)) +
   ylab(c("Recall")) +
   xlab("") +
   guides(fill=FALSE) +
   facet_grid(. ~ dataset, scales = "free", space = "free", switch="x") +
   theme(panel.background = element_rect(fill = "gray90"), axis.line = element_line(colour = "black"),
         axis.text.x=element_text(angle=45, hjust=1)) +
-  scale_fill_manual(values=c("light grey", "#F8766D", "#00BFC4"))
+  scale_fill_manual(values=c("light grey", "#F8766D", "#00BFC4")) +
+  stat_pvalue_manual(combined_ko_recall_wilcoxon, label = "p_symbol")
 
-pdf(file = "../../../figures/Supp_KO_precision_recall.pdf", width=16, height=8)
+pdf(file = "../../../figures/Supp_KO_precision_recall.pdf", width=16, height=10)
 
 plot_grid(ko_precision_boxplots,
           ko_recall_boxplots,
@@ -122,3 +124,23 @@ plot_grid(ko_precision_boxplots,
           ncol=1,
           labels=c("a", "b"))
 dev.off()
+
+
+
+# Calculate basic summary statistics.
+
+dataset_ko_acc_stats <- data.frame(matrix(NA, nrow=7, ncol=4))
+colnames(dataset_ko_acc_stats) <- c("precision_PICRUSt2_mean", "precision_PICRUSt2_sd", "recall_PICRUSt2_mean", "recall_PICRUSt2_sd")
+rownames(dataset_ko_acc_stats) <- datasets
+
+for(d in datasets) {
+  
+  d_name <- dataset2name[[d]]
+  
+  dataset_ko_acc_stats[d, ] <- c(mean(combined_ko_precision[which(combined_ko_precision$dataset == d_name & combined_ko_precision$cat == "NSTI=2"), "precision"]),
+                                 sd(combined_ko_precision[which(combined_ko_precision$dataset == d_name & combined_ko_precision$cat == "NSTI=2"), "precision"]),
+                                 mean(combined_ko_recall[which(combined_ko_recall$dataset == d_name & combined_ko_recall$cat == "NSTI=2"), "recall"]),
+                                 sd(combined_ko_recall[which(combined_ko_recall$dataset == d_name & combined_ko_recall$cat == "NSTI=2"), "recall"]))
+  
+}
+
