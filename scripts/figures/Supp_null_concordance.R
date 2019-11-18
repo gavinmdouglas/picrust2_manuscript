acced1d951a3bb816b5be94a3d109666c9e061d6
Gavin Douglas <gavinmdouglas@gmail.com>
2019-11-18 12:10:31 -0400

    Automatically write out supp null concordance

--- scripts/figures/Supp_null_concordance.R
+++ scripts/figures/Supp_null_concordance.R
@@ -7,6 +7,8 @@ combined_reps_melt <- readRDS(file = "/home/gavin/gavin_backup/projects/picrust2
 
 combined_reps_melt <- combined_reps_melt[-which(combined_reps_melt$num_genomes %in% c("2", "3", "4", "6", "7", "8", "9", "10", "75")), ]
 
+pdf(file = "../../../figures/Supp_null_concordance.pdf", width=8, height=5)
+
 ggplot(combined_reps_melt, aes(x=num_genomes, y=value, fill=db)) +
   geom_boxplot(outlier.shape = NA) +
   geom_quasirandom(size=1, dodge.width=0.8) +
@@ -14,8 +16,9 @@ ggplot(combined_reps_melt, aes(x=num_genomes, y=value, fill=db)) +
   scale_y_continuous(expand = c(0, 0), limits = c(0, 1)) +
   ylab("Spearman correlation coefficient") +
   xlab("Number of genomes") +
-  #scale_x_discrete(labels=gsub("n.", "", levels(combined_reps_melt$variable))) +
   guides(fill=guide_legend(title="Database")) +
   facet_grid(. ~ num_genomes, scales = "free", space = "free", switch="x") +
   theme(axis.text.x=element_blank(),
         axis.ticks.x=element_blank())
+
+dev.off()
