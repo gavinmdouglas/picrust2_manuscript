5a07a3550d3ac2d836f08f8ac18966bb00525c6a
Gavin Douglas <gavinmdouglas@gmail.com>
2020-02-23 14:59:32 -0400

    write out supp spreadsheet for DA validations

--- scripts/analyses/validations/16S_vs_MGS/write_DA_result_spreadsheet.R
+++ scripts/analyses/validations/16S_vs_MGS/write_DA_result_spreadsheet.R
@@ -1,12 +1,42 @@
 ### Commands to write the DA results to an excel spreadsheet, which is easier to explore.
 
+rm(list=ls(all.names=TRUE))
+
 library(openxlsx)
 
-full_DA <- list()
+setwd("/home/gavin/gavin_backup/projects/picrust2_manuscript/data/saved_RDS/DA_concordance/")
 
-for(genes in names(go_enrichment_results)) {
-  full_GO[[genes]] <- go_enrichment_results[[genes]]$all_results
+ko_da_out <- readRDS(file = "ko_da_out.rds")
+pathabun_da_out <- readRDS(file = "pathabun_da_out.rds")
+
+perf_list_to_dataframe <- function(in_list) {
+  
+  for(dataset in names(in_list)) {
+    orig_colnames <- colnames(in_list[[dataset]])
+    in_list[[dataset]][, "dataset"] <- dataset
+    in_list[[dataset]][, "metric"] <- rownames(in_list[[dataset]])
+    in_list[[dataset]] <- in_list[[dataset]][, c("dataset", "metric", orig_colnames)]
+  }
+   
+  combined_df <- do.call("rbind", in_list)
+  rownames(combined_df) <- NULL
+  
+  return(combined_df)
 }
 
-write.xlsx(full_GO, file = "tables/gene_set_full_GO_enrichment.xlsx")
+tmp <- ko_da_out$wilcoxon_musicc_out_perf_0.05)
+
+full_DA <- list(KO_wilcoxon_MUSICC_normalized=perf_list_to_dataframe(ko_da_out$wilcoxon_musicc_out_perf_0.05),
+                KO_ALDEx2=perf_list_to_dataframe(ko_da_out$aldex2_out_perf_0.05),
+                KO_DESeq2=perf_list_to_dataframe(ko_da_out$deseq2_out_perf_0.05),
+                KO_DESeq2_GMeans=perf_list_to_dataframe(ko_da_out$deseq2_GMmeans_out_perf_0.05),
+                KO_prevalence=perf_list_to_dataframe(ko_da_out$fishers_out_perf_0.05),
+                
+                pathabun_wilcoxon=perf_list_to_dataframe(pathabun_da_out$wilcoxon_out_perf_0.05),
+                pathabun_ALDEx2=perf_list_to_dataframe(pathabun_da_out$aldex2_out_perf_0.05),
+                pathabun_DESeq2=perf_list_to_dataframe(pathabun_da_out$deseq2_out_perf_0.05),
+                pathabun_DESeq2_GMeans=perf_list_to_dataframe(pathabun_da_out$deseq2_GMmeans_out_perf_0.05),
+                pathabun_prevalence=perf_list_to_dataframe(pathabun_da_out$fishers_out_perf_0.05))
+                
+write.xlsx(full_DA, file = "../../DA_validation_summary.xlsx")
 
